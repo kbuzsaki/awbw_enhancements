8780000b2b0eaa79e666c21a730bc6a4b96a30ef
Kyle Buzsaki <kbuzsaki@gmail.com>
2021-11-28 00:28:50 -0800

    Add ability to intercept savestate downloads and inject awbw helper metadata

--- background.js
+++ background.js
@@ -4,3 +4,26 @@ chrome.runtime.onInstalled.addListener(() => {
   chrome.storage.sync.set({ color });
   console.log('Default background color set to %cgreen', `color: ${color}`);
 });
+
+function shouldInterceptDownload(downloadItem) {
+    // Only intercept moveplanner blobs that don't have the modified tag.
+    return downloadItem.mime === "application/json"
+        && downloadItem.filename.endsWith("_moveplanner.json")
+        && downloadItem.finalUrl.startsWith("blob:")
+        && !downloadItem.finalUrl.endsWith("#awbw_helper_modified");
+}
+
+// Use onDeterminingFilename rather than onCreated so that we can get the filename
+chrome.downloads.onDeterminingFilename.addListener((downloadItem) => {
+    if (shouldInterceptDownload(downloadItem)) {
+        // Cancel the download and proxy its details to the content-script for rewriting
+        chrome.downloads.cancel(downloadItem.id);
+        chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
+            chrome.tabs.sendMessage(tabs[0].id, {
+                type: "savestate_download",
+                filename: downloadItem.filename,
+                finalUrl: downloadItem.finalUrl,
+            });
+        });
+    }
+});
--- property_counter_script.js
+++ property_counter_script.js
@@ -147,3 +147,28 @@ if (gamemap !== undefined) {
     throttler.handleUpdate();
 }
 
+// Handler for rewriting savestate downloads that are intercepted by the service worker.
+chrome.runtime.onMessage.addListener((message, sender) => {
+    if (message.type !== "savestate_download") return;
+
+    // Download the intercepted savestate ourselves
+    let xhr = new XMLHttpRequest();
+    xhr.addEventListener("load", (event) => {
+        // Parse the data out as json and add our extra data
+        // TODO: consider patching actual savestate data to fix the broken image bug?
+        let savestateData = JSON.parse(xhr.responseText);
+        // TODO: hook in actual awbw helper state that we need to persist
+        savestateData["awbw_helper_extras"] = {todo: "data goes here"};
+
+        // Then restart the download with a sentinel so that it isn't intercepted as well.
+        const url = window.URL.createObjectURL(
+            new Blob([JSON.stringify(savestateData)], {type: "application/json"}));
+        const a = document.createElement("a");
+        a.href = url + "#awbw_helper_modified";
+        a.download = message.filename;
+        document.body.appendChild(a);
+        a.click();
+    });
+    xhr.open("GET", message.finalUrl, true);
+    xhr.send();
+});
