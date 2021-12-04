ea3a7620f3b3d7cbe4bf22c45d87afb90bf28ab5
Kyle Buzsaki <kbuzsaki@gmail.com>
2021-12-03 23:31:45 -0800

    Remove capture tags for units that are on non-capturable properties

--- unitsinfo_patcher.js
+++ unitsinfo_patcher.js
@@ -7,14 +7,26 @@
                 unit.units_players_id = unit.players_id;
             }
 
-            // TODO: check if the unit is on a capturable tile?
+            // TODO: refine / de-jank this? Should players be able to force it?
             let span = document.getElementById("unit_" + unitId);
             if (span) {
                 let imgs = span.getElementsByTagName("img");
                 for (let img of imgs) {
                     if (img.src.indexOf("capture") !== -1) {
-                        console.log("patching units_capture for unit:", unit);
-                        unit.units_capture = 1;
+                        // Check if the capture is valid
+                        let building = buildingsInfo[unit.units_x][unit.units_y];
+                        if (!building) {
+                            console.log("removing capture indicator unit that is not on property:", unit);
+                            img.remove();
+                        } else if (unit.countries_code === building.countries_code) {
+                            console.log("removing capture indicator for owned property:", unit);
+                            img.remove();
+                        } else if (unit.units_capture !== 1) {
+                            console.log("patching units_capture for unit:", unit);
+                            unit.units_capture = 1;
+                        } else {
+                            // State matches up
+                        }
                     }
                 }
             }
@@ -24,6 +36,15 @@
     // Patch the unitsInfo data structure when units are added to the map.
     let gamemap = document.getElementById("gamemap");
     let observer = new MutationObserver((mutations, observer) => {
+        // Ignore cursor-only mutations
+        let isInteresting = false;
+        for (let mutation of mutations) {
+            if (mutation.target.id != "cursor") {
+                isInteresting = true;
+                break;
+            }
+        }
+
         patchUnitsInfo();
     });
     observer.observe(gamemap, {subtree: true, childList: true});
