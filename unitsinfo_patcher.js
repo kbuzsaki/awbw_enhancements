abfcaccb787a34b9287c2e5926d4fdc188966866
Kyle Buzsaki <kbuzsaki@gmail.com>
2021-12-03 20:20:50 -0800

    Patch unitsInfo on page load, not just when the gamemap is updated

--- unitsinfo_patcher.js
+++ unitsinfo_patcher.js
@@ -1,13 +1,21 @@
 (function() {
-    // Patch the unitsInfo data structure when units are added to the map.
-    let gamemap = document.getElementById("gamemap");
-    let observer = new MutationObserver((mutations, observer) => {
+    function patchUnitsInfo() {
         for (let unitId in unitsInfo) {
             let unit = unitsInfo[unitId];
             if (unit.hasOwnProperty("players_id") && !unit.hasOwnProperty("units_players_id")) {
+                console.log("patching units_players_id for unit:", unit);
                 unit.units_players_id = unit.players_id;
             }
-        };
+        }
+    }
+
+    // Patch the unitsInfo data structure when units are added to the map.
+    let gamemap = document.getElementById("gamemap");
+    let observer = new MutationObserver((mutations, observer) => {
+        patchUnitsInfo();
     });
     observer.observe(gamemap, {subtree: true, childList: true});
+
+    // Trigger initial patching even without any events.
+    patchUnitsInfo();
 })();
