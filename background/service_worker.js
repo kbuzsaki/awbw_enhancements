7491c6bc3b5f45e941f10f7e3b15a05e71259ec8
Kyle Buzsaki <kbuzsaki@gmail.com>
2021-12-12 14:38:32 -0800

    Simplify download interception to avoid round-tripping through service worker

--- background/service_worker.js
+++ background/service_worker.js
@@ -1,23 +1,11 @@
-chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-    if (message.type === "get_own_tab_id") {
-        sendResponse({tabId: sender.tab.id});
-    }
-});
-
 function classifyDownload(downloadItem) {
     // Only intercept moveplanner blobs that don't have the modified tag.
     let isSavestate = downloadItem.mime === "application/json"
                    && downloadItem.filename.endsWith("_moveplanner.json")
                    && downloadItem.finalUrl.startsWith("blob:");
-
     let isModified = downloadItem.finalUrl.endsWith("#awbw_helper_modified");
-    let tabId = undefined;
-    if (downloadItem.finalUrl.indexOf("#awbw_helper_unmodified") !== -1) {
-        let segments = downloadItem.finalUrl.split("-");
-        tabId = parseInt(segments[segments.length - 1]);
-    }
 
-    return {isSavestate, isModified, tabId};
+    return {isSavestate, isModified};
 }
 
 // Use onDeterminingFilename rather than onCreated so that we can get the filename
@@ -34,23 +22,13 @@ chrome.downloads.onDeterminingFilename.addListener((downloadItem) => {
 
         let classification = classifyDownload(downloadItem);
         console.log("classification", classification);
-        if (!classification.isSavestate || classification.isModified) {
-            // Ignore, it's either not a savestate or it's the final modified data to download
-            console.log("*** NOT intercepting download:", downloadItem);
-        } else if (classification.tabId === undefined) {
-            // It's an unidentified download, so cancel it outright
+        if (classification.isSavestate && !classification.isModified) {
+            // It's a savestate download that wasn't triggered by us, so cancel it
             console.log("Canceling unidentified savestate download");
             chrome.downloads.cancel(downloadItem.id);
         } else {
-            // It's an identified download that we need to intercept
-            console.log("Intercepting download", downloadItem, "from tab", classification.tabId);
-            // Cancel the download and proxy its details to the content-script for rewriting
-            chrome.downloads.cancel(downloadItem.id);
-            chrome.tabs.sendMessage(classification.tabId, {
-                type: "savestate_download",
-                filename: downloadItem.filename,
-                finalUrl: downloadItem.finalUrl,
-            });
+            // Ignore, it's either not a savestate or it's the final modified data to download
+            console.log("*** NOT intercepting download:", downloadItem);
         }
     });
 });
